699c542b127a1e01a8a04642b8c1753414c2b3ee
Kendry Grullon <kengrullon@gmail.com>
2023-11-14 15:49:57 -0400

    Fixed drag and drop.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,8 @@
 import { Form } from "@remix-run/react";
 import { ActionFunctionArgs } from "@remix-run/router";
-import { useState } from "react";
-import { useDropzone } from "react-dropzone";
+import clsx from "clsx";
+import { useCallback, useRef, useState } from "react";
+import { useDropzone } from "react-dropzone-esm";
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = await request.formData();
@@ -10,42 +11,55 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Index() {
-  const [files, setFiles] = useState([]);
+  const ref = useRef<HTMLInputElement>(null);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: { "image/*": [".png", ".jpg", ".jpeg", ".gif"] },
+    multiple: true,
+    async onDrop(acceptedFiles) {
+      console.log(acceptedFiles);
+    }
+  });
+
   return (
-    <section>
+    <section className="font-lato">
       <Form className="col-span-full">
         <label
           htmlFor="cover-photo"
           className="block text-sm font-medium leading-6 text-white"
         >
-          Cover photo
+          Images to convert
         </label>
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-titleText px-6 py-10">
+        <div
+          {...getRootProps()}
+          className={clsx(
+            "mt-2 flex justify-center rounded-lg border border-dashed px-32 py-10",
+            isDragActive ? "border-gray-400" : "border-titleText"
+          )}
+        >
           <div className="text-center">
-            <div className="mt-4 flex text-sm leading-6 text-gray-400">
+            <div className="mt-4 flex justify-center text-sm leading-6 text-gray-400">
               <label
                 htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-titleText focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500"
+                className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-titleText focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500 p-2"
               >
                 <span>Upload a file</span>
                 <input
-                  id="file-upload"
                   name="file-upload"
-                  type="file"
-                  accept="image/*"
-                  multiple
                   className="sr-only"
-                  onChange={(e) => {
-                    setFiles(e.currentTarget.files);
-                    console.log(e.currentTarget.files);
-                  }}
+                  ref={ref}
+                  {...getInputProps()}
                 />
               </label>
-              <p className="pl-1">or drag and drop</p>
             </div>
-            <p className="text-xs leading-5 text-titleText">
-              PNG, JPG, GIF up to 10MB
-            </p>
+            <div className="text-xs text-titleText">
+              <p className="">
+                or
+                <br /> drag and drop
+              </p>
+              <p className="text-xs leading-5 text-titleText">
+                PNG, JPG, GIF up to 5MB
+              </p>
+            </div>
           </div>
         </div>
       </Form>
