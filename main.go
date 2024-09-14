f581449f9483cfb681f00792a62cde372af18ae8
Kendry Grullon <kengrullon@gmail.com>
2024-09-13 21:15:00 -0400

    Cleaning up files after 2 mins of creation.

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+const STATIC_DIR = "files/queue/"
+
 func main() {
+	ticker := time.NewTicker(2 * time.Minute)
+
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("./static"))
+	doneFs := http.FileServer(http.Dir("./files/done"))
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fs))
+	mux.Handle("GET /files/done/", http.StripPrefix("/files/done/", doneFs))
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		t, err := template.ParseFiles("views/layout.html", "views/index.html")
@@ -36,12 +46,44 @@ func main() {
 			return
 		}
 		defer file.Close()
-		log.Println("------")
-		log.Println(header.Filename)
-		log.Println(header.Size)
-		log.Println("------")
+
+		dst, err := os.Create("files/queue/" + header.Filename)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+
+		if _, err := io.Copy(dst, file); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 	})
 
-	http.ListenAndServe(":9090", mux)
+	go http.ListenAndServe(":9090", mux)
+
+	for range ticker.C {
+		cleanupQueue()
+	}
+}
+
+func cleanupQueue() {
+	dir, err := os.ReadDir(STATIC_DIR)
+	if err != nil {
+		panic(err)
+	}
+	for _, entry := range dir {
+		if entry.IsDir() {
+			continue
+		}
+		fileName := fmt.Sprintf("%s/%s", STATIC_DIR, entry.Name())
+		err := os.Remove(fileName)
+		if err != nil {
+			log.Printf("error cleaning up file: %s", fileName)
+		}
+	}
 }
